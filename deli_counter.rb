2d33236a536458fe47478141e46b7293ddd6087d
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-13 03:03:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,27 @@
 # Write your code here.
 
-def line(x)
-  line_array = []
-  if x.length == 0
+def line(deli)
+  if deli.empty?
     puts "The line is currently empty."
   else
-    x.each.with_index(1) { |name, index|
-      line_array.push("#{index}. #{name}")
-    }
-    puts "The line is currently: #{line_array.join(" ")}"
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+
+def take_a_number(deli, name)
+  deli << name
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
-def now_serving(array)
-  if array.empty?
+
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array[0]}."
-    array.shift
+    puts "Currently serving #{deli.first}."
+    deli.shift
   end
 end
